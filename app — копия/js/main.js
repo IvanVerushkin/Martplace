00f0957b687b9df908655b32eb53c70e97d4bd57
IvanVerushkin <tenekrut30@gmail.com>
2020-07-22 21:21:48 -0300

    trying to do homework 10

--- app — копия/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-   $('.slider-wrapper').slick({
-      infinite: true,
-      dots: false,
-      arrows: true,
-      slidesToShow: 1,
-      slidesToScroll: 1
-   });
-});
--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,21 @@ $(function(){
       dots: false,
       arrows: true,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slick-arrow slick-arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
+      nextArrow: '<button type="button" class="slick-arrow slick-arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
+      appendArrows:$('.slider-control__buttons')
    });
+
+   $(".rate-star").rateYo({
+      rating: 5,
+      starWidth: "15px",
+      ratedFill: "#ffc000",
+      spacing: "3px"
+   });
+
+   var mixer = mixitup('.newproducts__items');
+
+
+
 });
